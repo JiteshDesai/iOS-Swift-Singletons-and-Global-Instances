b8c77c5b4e7b7d9783b441ba5e50a04fcf59bad7
Jitesh Desai <jitesh.m.desai@gmail.com>
2023-10-21 13:14:24 -0400

    Create Login Module
    added MockAPIClient
    added Login view controller
    added login api call into APIClient

--- Singletons and Global Instances.playground/Contents.swift
+++ Singletons and Global Instances.playground/Contents.swift
@@ -1,9 +1,28 @@
 import UIKit
 
-final class APIClient {
+struct LoggedInUser {}
+
+class APIClient {
     static let instance = APIClient()
     
     private init() {}
+    
+    func login(complition: (LoggedInUser) -> Void) {}
 }
 
 let client = APIClient.instance
+
+class MockAPIClient: APIClient {
+    
+}
+
+class LoginVC: UIViewController {
+    
+    var api = APIClient.instance
+    
+    func didTapLoginButton() {
+        api.login { user in
+                //show next screen
+        }
+    }
+}
